0a31ba5241b54d69713e89f90ef8f05011c2642d
CraftyYardBird <CraftyYardBird@github.com>
2018-04-04 00:40:18 -0400

    d

--- lesson-7/scripts/windchill.js
+++ lesson-7/scripts/windchill.js
@@ -15,19 +15,3 @@ document.getElementById("windTemp").innerHTML = chill.toFixed(2) + "&deg;F";
 } else {
 document.getElementById("windTemp").innerHTML = "Unavailable if average is over 50&deg;F or wind speed is under 3mph";
 }
-
- function computeNum() {
-	
-    var high = get("h").value;
-    var h = parseInt(high);
-    var low = get("l").value;
-    var l = parseInt(low);
-    var speed = get("s").value;
-    var s = parseInt(speed);
-    
-    var t = (h+l)/2;
-	
-	var wind = 35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16));
-	
-    get("output").innerHTML = wind.toFixed(2) ;
-}
